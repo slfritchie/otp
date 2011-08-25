7026026f37d29d149586863b564faee853a1927c
Scott Lystig Fritchie <slfritchie@snookles.com>
2011-08-24 21:04:21 -0500

    Add cpp safety () wrappers

--- erts/emulator/beam/dtrace-wrapper.h
+++ erts/emulator/beam/dtrace-wrapper.h
@@ -13,15 +13,15 @@
 #define DTRACE1(func, name, a0)                            \
     erlang_vm_##func##_##name(a0)
 #define DTRACE2(func, name, a0, a1)                        \
-    erlang_vm_##func##_##name(a0, a1)
+    erlang_vm_##func##_##name((a0), (a1))
 #define DTRACE3(func, name, a0, a1, a2)                    \
-    erlang_vm_##func##_##name(a0, a1, a2)
+    erlang_vm_##func##_##name((a0), (a1), (a2))
 #define DTRACE4(func, name, a0, a1, a2, a3)                \
-    erlang_vm_##func##_##name(a0, a1, a2, a3)
+    erlang_vm_##func##_##name((a0), (a1), (a2), (a3))
 #define DTRACE5(func, name, a0, a1, a2, a3, a4)            \
-    erlang_vm_##func##_##name(a0, a1, a2, a3, a4)
+    erlang_vm_##func##_##name((a0), (a1), (a2), (a3), (a4))
 #define DTRACE6(func, name, a0, a1, a2, a3, a4, a5)        \
-    erlang_vm_##func##_##name(a0, a1, a2, a3, a4, a5)
+    erlang_vm_##func##_##name((a0), (a1), (a2), (a3), (a4), (a5))
 
 #else   /* HAVE_DTRACE */
 
