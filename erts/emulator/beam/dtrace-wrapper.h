44aee0d9f8c2685d1be6d1ff5f49539d11964492
Scott Lystig Fritchie <slfritchie@snookles.com>
2011-11-14 15:07:31 -0600

    Intermediate checkin: save broken CentOS 5 systemtap 11th arg hack

--- erts/emulator/beam/dtrace-wrapper.h
+++ erts/emulator/beam/dtrace-wrapper.h
@@ -59,6 +59,55 @@ inline void dtrace_fun_decode(Process *process,
 #define DTRACE11(name, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) \
     erlang_##name((a0), (a1), (a2), (a3), (a4), (a5), (a6), (a7), (a8), (a9), (a10))
 
+#if defined(_SDT_PROBE) && !defined(STAP_PROBE11)
+/* SLF: This is Ubuntu 11-style SystemTap hackery */
+/* work arround for missing STAP macro */
+#define STAP_PROBE11(provider,name,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11) \
+  _SDT_PROBE(provider, name, 11, \
+             (arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11))
+#define _SDT_ASM_OPERANDS_11(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11) \
+  _SDT_ASM_OPERANDS_10(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9,arg10), \
+    _SDT_ARG(11, arg11)
+#endif
+
+#ifdef STAP_PROBE_ADDR
+/* SLF: This is CentOS 5-style SystemTap hackery */
+#if ! defined EXPERIMENTAL_KPROBE_SDT
+#define STAP_PROBE11(provider,probe,parm1,parm2,parm3,parm4,parm5,parm6,parm7,parm8,parm9,parm10,parm11) \
+  do { STAP_SDT_VOLATILE STAP_TYPE(parm1) arg1 = parm1;                 \
+  STAP_SDT_VOLATILE STAP_TYPE(parm2) arg2 = parm2;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm3) arg3 = parm3;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm4) arg4 = parm4;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm5) arg5 = parm5;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm6) arg6 = parm6;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm7) arg7 = parm7;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm8) arg8 = parm8;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm9) arg9 = parm9;                      \
+  STAP_SDT_VOLATILE STAP_TYPE(parm10) arg10 = parm10;                   \
+  STAP_SDT_VOLATILE STAP_TYPE(parm11) arg11 = parm11;                   \
+  STAP_PROBE_POINT(provider,probe, 11, "%0 %1 %2 %3 %4 %5 %6 %7 %8 %9 %10", arg11); \
+  } while (0)
+#else /* ! defined EXPERIMENTAL_KPROBE_SDT */
+#define STAP_PROBE11(provider,probe,parm1,parm2,parm3,parm4,parm5,parm6,parm7,parm8,parm9,parm10,parm11) \
+do { __extension__ struct {size_t arg1 __attribute__((aligned(8)));     \
+          size_t arg2 __attribute__((aligned(8)));                      \
+          size_t arg3 __attribute__((aligned(8)));                      \
+          size_t arg4 __attribute__((aligned(8)));                      \
+          size_t arg5 __attribute__((aligned(8)));                      \
+          size_t arg6 __attribute__((aligned(8)));                      \
+          size_t arg7 __attribute__((aligned(8)));                      \
+          size_t arg8 __attribute__((aligned(8)));                      \
+          size_t arg9 __attribute__((aligned(8)));                      \
+          size_t arg10 __attribute__((aligned(8)));                     \
+          size_t arg11 __attribute__((aligned(8)));}                    \
+  stap_probe11_args = {(size_t)parm1, (size_t)parm2, (size_t)parm3, (size_t)parm4, \
+        (size_t)parm5, (size_t)parm6, (size_t)parm7, (size_t)parm8, (size_t)parm9, (size_t)parm10, (size_t)parm11}; \
+  STAP_PROBE_DATA(provider,probe,STAP_GUARD,11);                        \
+  syscall (STAP_SYSCALL, #probe, STAP_GUARD, &stap_probe11_args);       \
+  } while (0)
+#endif /* ! defined EXPERIMENTAL_KPROBE_SDT */
+#endif /* STAP_PROBE_ADDR */
+
 #else   /* HAVE_DTRACE */
 
 /* Render all macros to do nothing */
@@ -75,16 +124,6 @@ inline void dtrace_fun_decode(Process *process,
                                                      do {} while (0)
 #define DTRACE11(name, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) \
                                                      do {} while (0)
-
-#if defined(_SDT_PROBE) && !defined(STAP_PROBE11)
-/* work arround for missing STAP macro */
-#define STAP_PROBE11(provider,name,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11) \
-  _SDT_PROBE(provider, name, 11, \
-             (arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11))
-#define _SDT_ASM_OPERANDS_11(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11) \
-  _SDT_ASM_OPERANDS_10(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9,arg10), \
-    _SDT_ARG(11, arg11)
-#endif
 
 #endif  /* HAVE_DTRACE */
 
