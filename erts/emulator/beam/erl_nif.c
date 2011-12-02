f4d2ff83f550244e5fa36472ad06231624b256cd
Scott Lystig Fritchie <slfritchie@snookles.com>
2011-12-01 21:03:06 -0600

    Move 2 USDT probes from dtrace_user.d -> erlang_dtrace.d
    This is a move that pollutes the Erlang VM with functions that are
    only used by the NIF shared library code in lib/dtrace/c_src/dtrace.c.
    Both probes are for use by Erlang code to fire DTrace probes, e.g.,
    `dtrace:p("Hello, world!")`.
    
    The reason for this not-so-wonderful move is that the current
    code (before this commit) does not work correctly on all supported
    platforms.  Here's the list of platforms and their before-this-commit
    status:
    
    	CentOS 5/SystemTap 1.3		Doesn't work
    	CentOS 6/SystemTap 1.2		Works fine
    	CentOS 6/SystemTap 1.4		Works fine
    	FreeBSD 9.0RC2			Works fine
    	OS X				Works fine
    	Solaris 10			Doesn't work
    	Solaris 11			Doesn't work
    	Open Solaris OpenIndiana 151a	Doesn't work
    	Illumos/SmartOS			Doesn't work
    
    I haven't figured out why the shared library probes don't work
    with SystemTap 1.3 but do work with 1.2 and 1.4.
    
    The Solaris issue is discussed in this `dtrace-discuss` mailing
    list thread:
    http://www.listbox.com/member/archive/184261/2011/12/sort/time_rev/page/1/entry/3:5/20111201185820:57584502-1C78-11E1-96DD-B85D9B43C33E/
    
    This commit moves the DTRACE_ENABLED() and DTRACE() macro invocations
    into statically linked code inside the virtual machine.  When the
    probes are fired via the NIF at their new location, the probes work
    correctly.

--- erts/emulator/beam/erl_nif.c
+++ erts/emulator/beam/erl_nif.c
@@ -32,6 +32,7 @@
 #include "error.h"
 #include "big.h"
 #include "beam_bp.h"
+#include "dtrace-wrapper.h"
 
 #include <limits.h>
 #include <stddef.h> /* offsetof */
@@ -1752,3 +1753,79 @@ static unsigned calc_checksum(unsigned char* ptr, unsigned size)
 
 #endif /* READONLY_CHECK */
 
+#ifdef HAVE_DTRACE
+
+#define MESSAGE_BUFSIZ 1024
+
+static void get_string_maybe(ErlNifEnv *env, const ERL_NIF_TERM term,
+                             char **ptr, char *buf, int bufsiz)
+{
+    ErlNifBinary str_bin;
+
+    if (!enif_inspect_iolist_as_binary(env, term, &str_bin) ||
+        str_bin.size > bufsiz) {
+        *ptr = NULL;
+    } else {
+        memcpy(buf, (char *) str_bin.data, str_bin.size);
+        buf[str_bin.size] = '\0';
+        *ptr = buf;
+    }
+}
+
+ERL_NIF_TERM erl_nif_user_trace_s1(ErlNifEnv* env, int argc,
+                                   const ERL_NIF_TERM argv[])
+{
+    ErlNifBinary message_bin;
+    DTRACE_CHARBUF(messagebuf, MESSAGE_BUFSIZ + 1);
+
+    if (DTRACE_ENABLED(user_trace_s1)) {
+	if (!enif_inspect_iolist_as_binary(env, argv[0], &message_bin) ||
+	    message_bin.size > MESSAGE_BUFSIZ) {
+	    return am_badarg;
+	}
+	memcpy(messagebuf, (char *) message_bin.data, message_bin.size);
+        messagebuf[message_bin.size] = '\0';
+	DTRACE1(user_trace_s1, messagebuf);
+	return am_true;
+    } else {
+	return am_false;
+    }
+}
+
+ERL_NIF_TERM erl_nif_user_trace_i4s4(ErlNifEnv* env, int argc,
+                                     const ERL_NIF_TERM argv[])
+{
+    DTRACE_CHARBUF(procbuf, 32 + 1);
+    DTRACE_CHARBUF(user_tagbuf, MESSAGE_BUFSIZ + 1);
+    char *utbuf = NULL;
+    ErlNifSInt64 i1, i2, i3, i4;
+    DTRACE_CHARBUF(messagebuf1, MESSAGE_BUFSIZ + 1);
+    DTRACE_CHARBUF(messagebuf2, MESSAGE_BUFSIZ + 1);
+    DTRACE_CHARBUF(messagebuf3, MESSAGE_BUFSIZ + 1);
+    DTRACE_CHARBUF(messagebuf4, MESSAGE_BUFSIZ + 1);
+    char *mbuf1 = NULL, *mbuf2 = NULL, *mbuf3 = NULL, *mbuf4 = NULL;
+
+    if (DTRACE_ENABLED(user_trace_i4s4)) {
+	dtrace_nifenv_str(env, procbuf);
+        get_string_maybe(env, argv[0], &utbuf, user_tagbuf, MESSAGE_BUFSIZ);
+        if (! enif_get_int64(env, argv[1], &i1))
+            i1 = 0;
+        if (! enif_get_int64(env, argv[2], &i2))
+            i2 = 0;
+        if (! enif_get_int64(env, argv[3], &i3))
+            i3 = 0;
+        if (! enif_get_int64(env, argv[4], &i4))
+            i4 = 0;
+        get_string_maybe(env, argv[5], &mbuf1, messagebuf1, MESSAGE_BUFSIZ);
+        get_string_maybe(env, argv[6], &mbuf2, messagebuf2, MESSAGE_BUFSIZ);
+        get_string_maybe(env, argv[7], &mbuf3, messagebuf3, MESSAGE_BUFSIZ);
+        get_string_maybe(env, argv[8], &mbuf4, messagebuf4, MESSAGE_BUFSIZ);
+	DTRACE10(user_trace_i4s4, procbuf, utbuf,
+		 i1, i2, i3, i4, mbuf1, mbuf2, mbuf3, mbuf4);
+	return am_true;
+    } else {
+	return am_false;
+    }
+}
+
+#endif /* HAVE_DTRACE */
--- erts/emulator/beam/erlang_dtrace.d
+++ erts/emulator/beam/erlang_dtrace.d
@@ -44,6 +44,37 @@
  */
 
 provider erlang {
+    /*
+     * Two probes for use by Erlang code ... moved to here from
+     * lib/dtrace/c_src/dtrace_user.d until a more portable solution
+     * is found; see commit log for details.
+     */
+    /**
+     * Send a single string to a probe.
+     *
+     * @param NUL-terminated string
+     */
+    probe user_trace__s1(char* message);
+
+    /**
+     * Multi-purpose probe: up to 4 NUL-terminated strings and 4
+     * 64-bit integer arguments.
+     *
+     * @param proc, the PID (string form) of the sending process
+     * @param user_tag, the user tag of the sender
+     * @param i1, integer
+     * @param i2, integer
+     * @param i3, integer
+     * @param i4, integer
+     * @param s1, string/iolist. D's arg6 is NULL if not given by Erlang
+     * @param s2, string/iolist. D's arg7 is NULL if not given by Erlang
+     * @param s3, string/iolist. D's arg8 is NULL if not given by Erlang
+     * @param s4, string/iolist. D's arg9 is NULL if not given by Erlang
+     */
+    probe user_trace__i4s4(char *proc, char *user_tag,
+                           int i1, int i2, int i3, int i4,
+                           char *s1, char *s2, char *s3, char *s4);
+
     /**
      * Fired when a message is sent from one local process to another.
      *
