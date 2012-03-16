160540ba9428991b749ccaccd25223fd671507be
Rick Reed <rr@whatsapp.com>
2012-03-16 11:52:23 -0700

    Patch mnesia_tm to dispatch async_dirty txns to separate procs for improved throughput.

--- lib/mnesia/src/mnesia_tm.erl
+++ lib/mnesia/src/mnesia_tm.erl
@@ -50,6 +50,8 @@
 	 system_code_change/4
 	]).
 
+-export([start_async_dirty_updater/1, async_dirty_updater/1, async_dirty_updater_loop/0]).
+
 -include("mnesia.hrl").
 -import(mnesia_lib, [set/2]).
 -import(mnesia_lib, [fatal/2, verbose/2, dbg_out/2]).
@@ -471,10 +473,56 @@ do_sync_dirty(From, Tid, Commit, _Tab) ->
     ?eval_debug_fun({?MODULE, sync_dirty, post}, [{tid, Tid}]),
     From ! {?MODULE, node(), {dirty_res, Res}}.
 
+do_async_dirty(Tid, Commit, _Tab) when Commit#commit.schema_ops == [],
+				       Commit#commit.snmp == [] ->
+    ?eval_debug_fun({?MODULE, async_dirty, pre}, [{tid, Tid}]),
+    catch do_async_dirty_update(Tid, Commit),
+    ?eval_debug_fun({?MODULE, async_dirty, post}, [{tid, Tid}]);
 do_async_dirty(Tid, Commit, _Tab) ->
     ?eval_debug_fun({?MODULE, async_dirty, pre}, [{tid, Tid}]),
     catch do_dirty(Tid, Commit),
     ?eval_debug_fun({?MODULE, async_dirty, post}, [{tid, Tid}]).
+
+do_async_dirty_update (Tid, Commit) ->
+    do_async_dirty_update_op(Tid, ram_copies, Commit#commit.ram_copies),
+    do_async_dirty_update_op(Tid, disc_copies, Commit#commit.disc_copies),
+    do_async_dirty_update_op(Tid, disc_only_copies, Commit#commit.disc_only_copies).
+
+do_async_dirty_update_op (Tid, Type, [{{Tab, _K}, _Obj, _OpType} = Op | Ops]) ->
+    Handler = list_to_atom("mnesia_tm_" ++ atom_to_list(Tab)),
+    case whereis(Handler) of
+	undefined ->
+	    case ?catch_val(Handler) of
+		{'EXIT', _} ->
+    		    mnesia_lib:set(Handler, ok),
+		    supervisor:start_child(mnesia_sup, {Handler, {?MODULE, start_async_dirty_updater, [Handler]}, transient, 5000, worker, [?MODULE]});
+		_ ->
+		    ok
+	    end,
+	    do_update(Tid, Type, [Op], ok);
+	Pid ->
+	    Pid ! {Tid, Type, Op}
+    end,
+    do_async_dirty_update_op(Tid, Type, Ops);
+do_async_dirty_update_op (_Tid, _Type, []) ->
+    ok.
+
+start_async_dirty_updater (Handler) ->
+    Pid = spawn_link(?MODULE, async_dirty_updater, [Handler]),
+    {ok, Pid}.
+
+async_dirty_updater (Handler) ->
+    register(Handler, self()),
+    async_dirty_updater_loop().
+
+async_dirty_updater_loop () ->
+    receive
+	{Tid, Type, Op} ->
+	    catch do_update(Tid, Type, [Op], ok);
+	_ ->
+	    ignore
+    end,
+    ?MODULE:async_dirty_updater_loop().
 
 
 %% Process items in fifo order
